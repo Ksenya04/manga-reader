59994cef277ffe19e654eb9e63020a135a642cfd
Харитонова Ксения <117857804+Ksenya04@users.noreply.github.com>
2023-03-05 13:34:49 +0000

    changes to font, background, and some text blocks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Offers can be discounted in <a href="https://vk.com/shio_mi">VK</a> developer</p>
 	</footer>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,49 +11,26 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+		<strong>Читайте мангу на сайте - Cacaomanga</strong>
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+		justify-content: left;
+		align-items: left;
 		flex: 0.6;
 	}
 
 	h1 {
 		width: 100%;
+		text-align: left;
+		font-size: 1.3rem;
+		font-style: Verdana;
+		color:aliceblue;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
